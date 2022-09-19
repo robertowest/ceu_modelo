cc8c17abe71fdeaafa9226d7c6e8c9091faefe53
Roberto West <roberto.west@gmail.com>
2022-09-19 12:50:24 +0200

    Add files via upload

---
+++ apps/tablon/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TablonConfig(AppConfig):
+    name = 'apps.tablon'
---
+++ apps/tablon/views.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def index(request):
+    # grados y posgrados
+    # http://desa01.ceu.es/Webservices/Uch/WebApiPlanesEstudiosWeb/api/PlanesEstudios?soloGradosPostgrados=true&soloPlanesConHitos=false
+    
+    # asignaturas
+    # {0} = IdPlanEstudioNK
+    # http://desa01.ceu.es/Webservices/Uch/WebApiPlanesEstudiosWeb/api/PlanesEstudios/v2/{0}?incluirPeriodos=false&incluirIdiomas=false&incluirHitos=false
+    
+    template = 'tablon/index.html'
+    context = {}
+    return render(request, template, context)
+
+
+def obtenerGrados()
+    resp = requests.get('http://desa01.ceu.es/Webservices/Uch/WebApiPlanesEstudiosWeb/api/Campus')
